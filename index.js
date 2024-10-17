b38229596d92ab24ab89ce0f9929cdadb2ffe15a
RahulDadhich26 <rahuldadhich80263@gmail.com>
2024-10-18 00:09:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/sum", function(req, res) {
+    console.log(req.name);
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans: a + b
+    })
+});
+
+app.listen(3000);
